9489447932cb4b1eb3d87b3415c544008d3f9c7c
yuchangyue1209 <changyue.yu@wisc.edu>
2025-08-02 16:30:14 -0500

    Clean up redundant scripts outside module structure

--- fasta_loader.py
+++
@@ -1,8 +0,0 @@
-# reference/fasta_loader.py
-from Bio import SeqIO
-
-def load_reference_fasta(fasta_path):
-    ref_seq = {}
-    for record in SeqIO.parse(fasta_path, "fasta"):
-        ref_seq[record.id] = str(record.seq).upper()
-    return ref_seq
